c866fead5203378cf1945e0adcfab83454539f0f
tangrobin <tangrobin@google.com>
2020-06-08 23:11:12 +0800

    sakura: use phase offsets as duration
    In Android R, the SurfaceFlinger calculate phase offsets
    based on duration. Adopt the new way of specifying
    phase offsets to SurfaceFlinger.
    
    Bug: 158464992
    Test: scrolling, app launching/closing, playing video
    Change-Id: Ia717f5c0075b6adaedaf20dd6be689737edc1415
    Signed-off-by: GeoPD <geoemmanuelpd2001@gmail.com>
    signed-off-by: Zabuka

--- prop.mk
+++ prop.mk
@@ -208,10 +208,14 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 ro.surface_flinger.protected_contents=true
 
 PRODUCT_PROPERTY_OVERRIDES += \
-debug.sf.early_phase_offset_ns=1500000 \
-debug.sf.early_app_phase_offset_ns=1500000 \
-debug.sf.early_gl_phase_offset_ns=3000000 \
-debug.sf.early_gl_app_phase_offset_ns=15000000
+debug.sf.use_phase_offsets_as_durations=1 \
+debug.sf.late.sf.duration=10500000 \
+debug.sf.late.app.duration=20500000 \
+debug.sf.early.sf.duration=21000000 \
+debug.sf.early.app.duration=16500000 \
+debug.sf.earlyGl.sf.duration=13500000 \
+debug.sf.earlyGl.app.duration=21000000
+
 
 # Time Services
 PRODUCT_PROPERTY_OVERRIDES += \
