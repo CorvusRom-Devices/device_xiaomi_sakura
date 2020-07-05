51218f2dd915e448e794b696b1c1dd8728984a2d
0ranko0P <ranko0p@outlook.com>
2020-07-06 01:54:07 +0800

    sakura: Set zygote preforking in system_prop
    Move it to PRODUCT_SYSTEM_DEFAULT_PROPERTIES to fix violated by
    allow vendor_init device_config_runtime_native_prop:property_service { set };
    
    Test: boot and getprop
    Test: logcat USAP pool
    Signed-off-by: Zabuka <yusufnoor786@gmail.com>

--- prop.mk
+++ prop.mk
@@ -239,5 +239,5 @@ PRODUCT_PROPERTY_OVERRIDES += \
 wifi.interface=wlan0
 
 # Zygote preforking
-PRODUCT_PROPERTY_OVERRIDES += \
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 persist.device_config.runtime_native.usap_pool_enabled=true
