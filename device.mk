acf4fee9533f04d9048229edde589bc65bf43b1f
Gagan Malvi <malvigagan@gmail.com>
2020-10-20 07:29:18 +0000

    sakura: copy prebuilt vndk 29
    Signed-off-by: AlexThundrous <alexthundrous2104@gmail.com>

--- device.mk
+++ device.mk
@@ -68,3 +68,7 @@ PRODUCT_COPY_FILES += \
 # Touch HAL
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.xiaomi_8953
+#Prebuilts
+PRODUCT_COPY_FILES += \
+		            prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full.so \
+			         prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-lite.so
