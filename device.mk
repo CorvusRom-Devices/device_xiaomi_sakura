e8e7081fa33a1d879a21c17b609e93339c3d37ef
Alex Thundrous <alexthundrous2104@gmail.com>
2020-11-09 11:08:32 +0530

    Add back lineage-touch Also delete prebuilts
    Already add in msm8953 tree

--- device.mk
+++ device.mk
@@ -62,7 +62,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf
 
-#Prebuilts
-PRODUCT_COPY_FILES += \
-		            prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full.so \
-			         prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-lite.so
+#Lineage Touch
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch@1.0-service.xiaomi_8953
