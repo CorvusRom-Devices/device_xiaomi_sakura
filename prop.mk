d13c1317fb649c7bc116e2fac12bc5bd2725367c
“yusufnoor786” <yusufnoor786@gmail.com>
2020-12-18 09:12:40 +0000

    Revert "sakura: use phase offsets as duration"
    This reverts commit c866fead5203378cf1945e0adcfab83454539f0f.
    
    Signed-off-by: Zabuka <yusufnoor786@gmail.com>

--- prop.mk
+++ prop.mk
@@ -214,14 +214,10 @@ ro.surface_flinger.set_touch_timer_ms=5000 \
 ro.surface_flinger.use_smart_90_for_video=true
 
 PRODUCT_PROPERTY_OVERRIDES += \
-debug.sf.use_phase_offsets_as_durations=1 \
-debug.sf.late.sf.duration=10500000 \
-debug.sf.late.app.duration=20500000 \
-debug.sf.early.sf.duration=21000000 \
-debug.sf.early.app.duration=16500000 \
-debug.sf.earlyGl.sf.duration=13500000 \
-debug.sf.earlyGl.app.duration=21000000
-
+debug.sf.early_phase_offset_ns=1500000 \
+debug.sf.early_app_phase_offset_ns=1500000 \
+debug.sf.early_gl_phase_offset_ns=3000000 \
+debug.sf.early_gl_app_phase_offset_ns=15000000
 
 # Time Services
 PRODUCT_PROPERTY_OVERRIDES += \
