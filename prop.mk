b469cf87a98f1a3c3363ac6416c9a82a41676279
Andrzej Perczak <kartapolska@gmail.com>
2021-07-13 16:53:19 +0530

    sakura: Explicitly enable IORap and IORapd prefetching * reduces app startup times
    Signed-off-by: ManshuTyagi <himanshut567@gmail.com>
    Signed-off-by: YadavMohit19 <mohityadav050575@gmail.com>

--- prop.mk
+++ prop.mk
@@ -144,6 +144,11 @@ ro.fm.transmitter=false
 PRODUCT_PROPERTY_OVERRIDES += \
 ro.frp.pst=/dev/block/bootdevice/by-name/config
 
+# Iorap
+PRODUCT_PROPERTY_OVERRIDES += \
+   iorapd.perfetto.enable=true \
+   iorapd.readahead.enable=true
+
 # Graphics
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.hardware.egl=adreno \
