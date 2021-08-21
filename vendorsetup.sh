0cce605cce4ff9e4cd7cccce6931ca3b0a0527eb
YadavMohit19 <mohityadav050575@gmail.com>
2021-08-21 14:01:15 +0530

    sakura: Add vendorsetup.sh for msm8996 hals Signed-off-by: YadavMohit19 <mohityadav050575@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,4 @@
+# msm8996 caf-hals
+git clone https://github.com/LineageOS/android_hardware_qcom_display.git -b lineage-18.1-caf-msm8996 hardware/qcom-caf/msm8996/display
+git clone https://github.com/LineageOS/android_hardware_qcom_audio.git -b lineage-18.1-caf-msm8996 hardware/qcom-caf/msm8996/audio
+git clone https://github.com/LineageOS/android_hardware_qcom_media.git -b lineage-18.1-caf-msm8996 hardware/qcom-caf/msm8996/media
