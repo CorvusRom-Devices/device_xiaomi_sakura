e89504384cdc9a660727020dd4a9eadeda49ea7e
clarencelol <clarencekuiek@icloud.com>
2022-02-13 20:06:23 +0800

    sakura: properties: Disable SDM Scalar
    * Else, ScalarConfig: :GetPipeScaleSettingsInfo: lib_scale_get_pipe_settings failed: status = 2
    
    Change-Id: I7ee49c90f7ad948641d81b3c261351a6b3728fed
    Signed-off-by: YadavMohit19 <mohityadav050575@gmail.com>

--- prop.mk
+++ prop.mk
@@ -126,6 +126,7 @@ ro.qualcomm.cabl=0 \
 ro.sf.lcd_density=420 \
 ro.vendor.display.cabl=2 \
 sdm.debug.disable_skip_validate=1 \
+vendor.display.disable_scaler=1 \
 vendor.display.disable_skip_validate=1 \
 vendor.display.enable_default_color_mode=0 \
 vendor.gralloc.enable_fb_ubwc=1
