bdca955f1e7e311cafbee6f49eec045ba3148bab
kubersharma001 <kubersharma001@gmail.com>
2022-03-06 13:51:34 +0530

    sakura: Don't inherit updatable_apex.mk
    This is because of the following reasons:
    - Many build scripts force disable it anyways by settings OVERRIDE_TARGET_FLATTEN_APEX env flag so why bother rendering differences in official and local builds [1][2].
    - Compressed apex causes issues with OTA on A only devices [3].
    - Compressed apex may cause issues on devices which do not meet kernel requirements [4].
    - Highly anticipated mainline modules update aka Play System updates is limited to OEMs/Pixels only [5], we don't do such things on AOSP; shipping Pixel prebuilt apex components feels somewhat unethical.
    
    Ref:
    [1]: https://github.com/lineageos-infra/build-config/blob/8e435ce7ff7c74097b3500e143380273e099aefb/android/build.sh#L9
    [2]: https://github.com/LineageOS/android_build/blob/lineage-18.1/target/product/updatable_apex.mk#L19
    [3]: RecoverySystemService: Failed to reserve space for compressed apex
    [4]: https://source.android.com/devices/tech/ota/apex#kernel-requirements
    [5]: "pushed to end-user devices, either by Google (using the Google Play Store infrastructure) or by the Android partner (using a partner-provided OTA mechanism)." as stated in https://source.android.com/devices/architecture/modular-system#architecture
    
    test: m, boots and ota.
    Signed-off-by: YadavMohit19 <mohityadav050575@gmail.com>

--- device.mk
+++ device.mk
@@ -18,9 +18,6 @@ $(call inherit-product, vendor/xiaomi/sakura/sakura-vendor.mk)
 $(call inherit-product, vendor/xiaomi/msm8953-common/msm8953-common-vendor.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Enable updating of APEXes
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
