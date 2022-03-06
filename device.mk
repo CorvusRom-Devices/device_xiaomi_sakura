ccfd6f24195695c3fa5323e62c6bdfaabf6b425c
YadavMohit19 <mohityadav050575@gmail.com>
2022-03-06 14:02:18 +0530

    sakura: Drop useless vndk packages & Build Build Protobuf Vendorcompat Lib
    Signed-off-by: YadavMohit19 <mohityadav050575@gmail.com>

--- device.mk
+++ device.mk
@@ -494,11 +494,8 @@ PRODUCT_PACKAGES += \
     libstdc++.vendor \
     com.android.vndk.current.on_vendor
 
-PRODUCT_COPY_FILES += \
-    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-lite.so \
-    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full.so
-
 PRODUCT_PACKAGES += \
+    libprotobuf-cpp-full \
     libprotobuf-cpp-full-vendorcompat \
     libprotobuf-cpp-lite-vendorcompat
 
