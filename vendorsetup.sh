811458bccfa50880032cbe15989caefb1ba26cba
YadavMohit19 <mohityadav050575@gmail.com>
2022-04-28 13:11:46 +0530

    sakura: Update vendorsetup.sh
    Signed-off-by: YadavMohit19 <mohityadav050575@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,9 @@
-# msm8996 caf-hals
-git clone https://github.com/LineageOS/android_hardware_qcom_display.git -b lineage-18.1-caf-msm8996 hardware/qcom-caf/msm8996/display
-git clone https://github.com/LineageOS/android_hardware_qcom_audio.git -b lineage-18.1-caf-msm8996 hardware/qcom-caf/msm8996/audio
-git clone https://github.com/LineageOS/android_hardware_qcom_media.git -b lineage-18.1-caf-msm8996 hardware/qcom-caf/msm8996/media
+cd hardware/qcom-caf/msm8996
+rm -rf audio
+rm -rf display
+rm -rf media
+cd ../../..
+
+git clone https://github.com/YadavMohit19/hardware_qcom-caf -b audio-msm8996-s hardware/qcom-caf/msm8996/audio
+git clone https://github.com/YadavMohit19/hardware_qcom-caf -b display-msm8996-s hardware/qcom-caf/msm8996/display
+git clone https://github.com/YadavMohit19/hardware_qcom-caf -b media-msm8996-s hardware/qcom-caf/msm8996/media
