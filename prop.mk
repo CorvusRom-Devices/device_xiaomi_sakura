ef2020da5c9d40ab6ab447fac6ef46fa961e8bb7
Vivekachooz <vivekachooz@gmail.com>
2022-06-16 11:48:58 +0530

    sakura: Update sf offset values from raven * From raven-user-12-SQ1D.220205.003-8069835-release-keys
    Change-Id: I2ce1198ca9011a736c4516d520b06215bbff396c
    Signed-off-by: YadavMohit19 <mohityadav050575@gmail.com>

--- prop.mk
+++ prop.mk
@@ -293,11 +293,11 @@ ro.surface_flinger.use_smart_90_for_video=true
 PRODUCT_PROPERTY_OVERRIDES += \
 debug.sf.use_phase_offsets_as_durations=1 \
 debug.sf.late.sf.duration=10500000 \
-debug.sf.late.app.duration=20500000 \
-debug.sf.early.sf.duration=16000000 \
-debug.sf.early.app.duration=16500000 \
-debug.sf.earlyGl.sf.duration=13500000 \
-debug.sf.earlyGl.app.duration=21000000
+debug.sf.late.app.duration=16600000
+debug.sf.early.sf.duration=16600000
+debug.sf.early.app.duration=16600000
+debug.sf.earlyGl.sf.duration=16600000
+debug.sf.earlyGl.app.duration=16600000
 
 # System
 PRODUCT_PROPERTY_OVERRIDES += \
